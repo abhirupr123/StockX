1ffc38d53d4e346446c07d5cfdcc24f8d026dec6
unknown <chandraniroy.sk@gmail.com>
2023-05-07 23:58:40 +0530

    Fixed API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Container, HStack, Img, VStack, Text, Heading } from '@chakra-ui/react';
+import { Container, HStack, VStack, Text, Heading } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Loader from './Loader';
 
 const Home = () => {
   
+  const options = {
+    method: 'GET',
+    url: 'https://crypto-news16.p.rapidapi.com/news/top/20',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_MY_API_KEY,
+      'X-RapidAPI-Host': process.env.REACT_APP_MY_HOST
+    }
+  };
+  
   const [news, setNews]=useState([]);
   const [loading,setLoading]=useState(true);
   useEffect(() => {
     const fetchNews= async() =>{
-    const {data}= await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${process.env.REACT_APP_MY_API_KEY}`);  
-    setNews(data.articles);
+    const {data}= await axios.request(options);  
+    setNews(data);
     setLoading(false);
   };
     fetchNews();
@@ -29,7 +38,7 @@ const Home = () => {
             <NewsCard
             key={i.title}
             title={i.title}
-            img={i.urlToImage}
+            des={i.description}
             url={i.url}
             />
           ))}
@@ -40,13 +49,13 @@ const Home = () => {
   );
 };
 
-const NewsCard=({title,img,url}) =>(
+const NewsCard=({title,des,url}) =>(
 
 <a href={url} target={"blank"}>
 <VStack w={52} p={8} shadow={"lg"} borderRadius={"lg"} m={4} transition={"all 0.3s"}
 css={{"&:hover":{transform:"scale(1.1)"}}}>
-  <Img src={img} w={160} h={20}/>
-  <Text noOfLines={3}>{title}</Text>
+  <Text noOfLines={3} fontWeight={'bold'}>{title}</Text>
+  <Text noOfLines={3}>{des}</Text>
 </VStack>
 </a>
 );
